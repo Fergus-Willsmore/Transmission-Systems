a696c1f24c5991f4d9d0e5a1312c63cbfabca7f2
Fergus Willsmore <ferguswillsmore@cmcs-iMac.local>
2018-03-16 12:02:36 +1030

    Update

---
+++ topology/rand.net.top.R
@@ -0,0 +1,66 @@
+# Random Network Topology (rand.net.top)
+# A function that will randomly connect components of a network until a connected network is reached.
+# This function works with the igraph library to manipulate the network structure.
+# Input: An igraph network object (presumably not connected).
+# Output: An igraph network object that is connected.
+# Created by F.R. Willsmore 16/3/2018
+
+rand.net.top <- function(net){
+  
+  # Check for a connected network
+  
+  if (components(net)$no==1) stop('The network is already connected.')
+  
+  # Precautionary measure for while loop
+  
+  count = 0
+  
+  repeat{
+    
+    # Decompose network into a list of network components
+    
+    net.components <- decompose(net, min.vertices=2)
+    
+    # Random order of components
+    
+    rcomp <- sample(1:length(net.components),length(net.components))
+    
+    # Size of each component
+    
+    len <- sapply(rcomp, function(x) length(vertex_attr(net.components[[x]])$name))
+    
+    # choose a random bus in each component
+    
+    rbus <- sapply(len, function(x) sample(1:x,1))
+    
+    # Combine components and bus into a list
+    
+    rnum <- cbind(rcomp, rbus)
+    rls<-split(rnum, row(rnum))
+    
+    # obtain the random bus name for each component
+    
+    busnames <- sapply(rls, function(x) vertex_attr(net.components[[x[1]]])$name[x[2]]) %>% matrix(ncol=2)
+    
+    # create an edgelist of paths between components
+    
+    paths <- graph_from_edgelist(busnames,directed = F)
+    
+    # Update the network to be the union of the network and the paths
+    
+    net <- net %u% paths
+    
+    # Stop if network is connected or too many iterations
+    
+    if (components(net)$no==1|count==30){
+      break
+    }
+    
+    # update count
+    
+    count = count+1
+  }
+  
+  return(net)
+  
+}
--- topology/top_test.R
+++ topology/top_test.R
@@ -1,4 +1,7 @@
-# Test topology algorithm using Fake Data with name of 'A-B' format
+# Create a Connected Network
+# Test topology algorithm using line data in 'bus-bus' format
+# The line data is from planned outage data in PUBLIC_NOSDAILY_2018030515000037 copy.csv, 
+# processed via python script topology.py to obtain 'bus-bus' format.
 # Created by F.R. Willsmore 6/2/2018
 
 # Import packages
@@ -6,87 +9,48 @@
 library(tidyverse)
 library(stringr)
 library(igraph)
+library(ggraph)
+library(ggplot2)
 library(statnet)
 
 components <- igraph::components
 
-# Simple small check
-name <- c('Armidale-Avon', 'Armidale-Bannaby','Avon-Burrinjuck','Armidale-Burrinjuck')
-name.ls <- strsplit(name,'-')
-el <- matrix(unlist(name.ls), ncol = 2, byrow = TRUE)
-net <- graph_from_edgelist(el,directed = F)
-adj <- get.adjacency(net)
-
-# Slightly larger check
-
-# Read csv data with headers and one intro line
+# Read in outage data if needed
 
 outage.df <- read.csv('~/Documents/GitHub/Transmission-Systems/topology/PUBLIC_NOSDAILY_2018030515000037 copy.csv', 
                       header = TRUE, sep = ",", skip = 1)
 
-bus <- outage.df$Station.Name
-bus <- as.character(bus[-which(duplicated(bus))])
-bus <- unlist(list(lapply(bus,function(x) gsub("[0-9]{3}kV","",x))))
+# Read .txt file containing the line names processed by topology.py
 
 lines <- scan('~/Documents/GitHub/Transmission-Systems/topology/top_test.txt', character(), quote = "")
+
+# Create edge-list for network
+
 name.ls <- strsplit(lines,'-')
 el <- matrix(unlist(name.ls), ncol = 2, byrow = TRUE)
+
+# Create network from edglist
+
 net <- graph_from_edgelist(el,directed = F)
-adj <- get.adjacency(net)
+
+# Plot the initial network
 
 plot(net,layout=layout_with_fr, vertex.size=4,
      vertex.label.dist=0.5, vertex.color="red",vertex.label=NA)
 
 ####### Connectivity ##########
 
-set.seed(344)
-count = 0
+set.seed(560)
 
-repeat{
-  
-  # Decompose network into a list of network components
-  
-  components <- decompose(net, min.vertices=2)
-  
-  # Random order of components
-  
-  rcomp <- sample(1:length(components),length(components))
-  
-  # Size of each component
-  
-  len <- sapply(rcomp, function(x) length(vertex_attr(components[[x]])$name))
-  
-  # choose a random bus in each component
-  
-  rbus <- sapply(len, function(x) sample(1:x,1))
-  
-  # Combine components and bus into a list
-  
-  rnum <- cbind(rcomp, rbus)
-  rls<-split(rnum, row(rnum))
-  
-  # obtain the random bus name for each component
-  
-  busnames <- sapply(rls, function(x) vertex_attr(components[[x[1]]])$name[x[2]]) %>% matrix(ncol=2)
-  
-  # create an edgelist of paths between components
-  
-  paths <- graph_from_edgelist(busnames,directed = F)
-  
-  # Update the network to be the union of the network and the paths
-  
-  net <- net %u% paths
-  
-  # Stop if network is connected
-  
-  if (igraph::components(net)$no==1|count==30){
-    break
-  }
-  count = count+1
-}
+con.net <- rand.net.top(net)
 
-plot(net,layout=layout_with_fr, vertex.size=4,
-     vertex.label.dist=0.5, vertex.color="red",vertex.label=NA)
+ggraph(con.net) + 
+  geom_edge_link() + 
+  geom_node_point(col = "indianred1", size = 1) +
+  theme_graph()
+
+plot(con.net,layout=layout_with_fr, vertex.size=3,
+     vertex.label.dist=0.5, vertex.color="indianred1",vertex.label=NA)
 
 
 
